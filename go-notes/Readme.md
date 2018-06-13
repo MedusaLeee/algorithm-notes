0a258c7036cd3821414bc2a50ed5c86fd8e29197
jianxun <medusa.leee@gmail.com>
2018-06-13 23:33:23 +0800

    增加golang二分查找

---
+++ go-notes/Readme.md
@@ -0,0 +1,3 @@
+# 算法学习笔记-golang实现
+
+* [二分查找](./binarySearch.go)
---
+++ go-notes/binarySearch.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func binarySearch(s []int, k int)  int {
+	low := 0
+	high := len(s) -1
+	for low < high{
+
+		var mid int = (low + high) /2
+		guess := s[mid]
+		if guess == k {
+			return mid
+		}
+		if guess > k {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return -1
+}
+
+func main()  {
+	arr := []int{1,2,3,4,5}
+	result := binarySearch(arr, 3)
+	fmt.Println(result)
+	result = binarySearch(arr, 4)
+	fmt.Println(result)
+	result = binarySearch(arr, 6)
+	fmt.Println(result)
+}
