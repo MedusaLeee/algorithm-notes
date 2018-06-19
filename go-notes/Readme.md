28e495686ffc0da29b7afc0a233f8bac88ca91c3
jianxun <medusa.leee@gmail.com>
2018-06-19 21:22:32 +0800

    增加使用递归进行数组求和示例

--- go-notes/Readme.md
+++ go-notes/Readme.md
@@ -1,3 +1,4 @@
 # 算法学习笔记-golang实现
 
 * [二分查找](./binarySearch.go)
+* [使用递归进行数组求和-算法图解练习4.1](./arraySum.go)
---
+++ go-notes/arraySum.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func ArraySum(s [] int, index int) int {
+	length := len(s)
+	if index == length-1 {
+		return s[index]
+	}
+	return s[index] + ArraySum(s, index+1)
+}
+
+func main() {
+	slice := []int{1, 2, 3, 4, 5}
+	sum := ArraySum(slice, 0)
+	fmt.Println(sum)
+}
